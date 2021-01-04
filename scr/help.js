8bbb75d5a4ece3fce9b2156b6cca12bf7750c601
gaarazinho <76838261+gaarazinho@users.noreply.github.com>
2021-01-04 02:58:12 -0300

    Add files via upload

---
+++ scr/help.js
@@ -0,0 +1,113 @@
+const help = (prefix) => {
+	
+
+
+ 
+╔════════════════════
+║        モℜモղ ɮロで ツ
+╠════════════════════
+║
+╠════✪〘 𝗙𝗜𝗚𝗨𝗥𝗜𝗡𝗛𝗔 〙✪═════
+║
+╠➣ COMANDO: .sticker
+╠➣ DESC: Converter em figurinha
+╠➣ USO: Converter em figurinha
+
+║
+╠➣ COMANDO: .sticker nobg
+╠➣ DESC: Imagem em figurinha sem fundo
+╠➣ USO:  Enviar imagem com legenda
+
+║
+╠➣ COMANDO: .toimg
+╠➣ DESC: Converter figurinha em imagem
+╠➣ USO: Figurinha de resposta
+
+║
+╠➣ COMANDO: .tsticker
+╠➣ DESC: Converter texto em Figurinha
+╠➣ USO: !tsticker [seu texto]
+
+║
+╠═════✪〘 𝗠𝗘𝗠𝗘 〙✪══════
+║
+╠➣ COMANDO: .meme
+╠➣ DESC: Imagens aleatórias de meme(english)
+╠➣ USO: Apenas envie o comando
+
+║
+╠➣ COMANDO: .memeindo
+╠➣ DESC: Imagens aleatórias de meme [indo]
+╠➣ USO: Apenas envie o comando
+
+║
+╠═════✪〘 𝗢𝗨𝗧𝗥𝗢𝗦 〙✪═════  
+║
+╠➣ COMANDO: .gtts
+╠➣ DESC: Fazer o bot gravar áudio
+╠➣ USO: .gtts [cc] [text]
+
+╠➣ EXEMPLO: .gtts ja On2-chan
+
+║
+╠➣ COMANDO: .loli
+╠➣ DESC: Imagens de loli
+╠➣ USO: Apenas envie o comando
+
+║
+╠➣ COMANDO: .nsfwloli
+╠➣ DESC: Imagens de nsfw loli
+╠➣ USO: Apenas envie o comando
+
+║
+╠➣ COMANDO: .url2img
+╠➣ DESC: Tirar screenshots da web
+╠➣ USO: !url2img [tipe] [url]
+
+║
+╠➣ COMANDO: .simi
+╠➣ DESC: Sua mensagem será respondida
+╠➣ USO: .simi [sua mensagem]
+
+║
+╠➣ COMANDO: .ocr
+╠➣ DESC: Pegue o texto na foto
+╠➣ USO: Enviar imagem com legenda
+
+║
+╠➣ COMANDO: .wait
+╠➣ DESC: Procure anime [Que anime é]
+╠➣ USO: Enviar imagem com legenda n
+║
+╠➣ COMANDO: *.setprefix
+╠➣ DESC: Substituir prefixo
+╠➣ USO: !setprefix [text|optional]
+
+╠➣ EXEMPLO: .setprefix /
+╠➣ NOTA: Usado  pelo proprietário do bot
+║
+╠═════✪〘 𝗚𝗥𝗨𝗣𝗢 〙✪═════
+║
+╠➣ COMANDO: .linkgroup
+╠➣ DESC: Enviar o link do grupo
+╠➣ USO: Apenas envie o comando
+╠➣ NOTA: Usado quando o bot é admin!
+
+║
+╠➣ COMANDO: .tagall
+╠➣ DESC: Marca todos do grupo
+╠➣ USO: Apenas envie o comando
+╠➣ NOTA: Usado por adms do grupo!
+
+║
+╠➣ COMANDO: .simih
+╠➣ DESC: Ative o modo simi no grupo
+╠➣ ATIVAR: .simih 1 
+╠➣ DESATIVAR: .simih 0
+╠➣ NOTA: Usado quando você é adm do grupo
+
+║
+╠════════════════════
+║    𝐁𝐘 ᏀᎪᎪᏒᎪ O ᏴᎡᎪᏴᎾ 🤠🤙
+╚════════════════════
+
